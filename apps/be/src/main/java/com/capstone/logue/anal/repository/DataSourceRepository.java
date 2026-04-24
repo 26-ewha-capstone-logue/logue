cb3733e29c44f9655507c9634f08c5ac947c9fd6
jeein <23minjjj@ewha.ac.kr>
2026-04-24 17:10:35 +0900

    refactor: anal/repository 아래의 DataSourceRepository 삭제 후 data/repository 아래의 것 사용

--- apps/be/src/main/java/com/capstone/logue/anal/repository/DataSourceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.capstone.logue.anal.repository;
-
-import com.capstone.logue.global.entity.DataSource;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DataSourceRepository extends JpaRepository<DataSource, Long> {
-}
