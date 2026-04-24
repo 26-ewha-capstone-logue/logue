ed9687aa54d75f835fe3ea0258a643bdb571d430
jeein <23minjjj@ewha.ac.kr>
2026-04-24 18:27:49 +0900

    chore: json 직렬화 위한 코드 추가

--- apps/be/src/main/java/com/capstone/logue/global/config/AppConfig.java
+++ apps/be/src/main/java/com/capstone/logue/global/config/AppConfig.java
@@ -3,6 +3,7 @@ package com.capstone.logue.global.config;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,6 +13,8 @@ public class AppConfig {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
+        return builder
+                .messageConverters(new MappingJackson2HttpMessageConverter())
+                .build();
     }
 }
