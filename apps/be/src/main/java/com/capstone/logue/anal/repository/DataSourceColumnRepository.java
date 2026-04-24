e27e91f90898e14ecc3da2a9059c71621a4593dc
jeein <23minjjj@ewha.ac.kr>
2026-04-24 17:58:52 +0900

    feat: 비동기 작업 구현 위한 repository 함수 구현

--- apps/be/src/main/java/com/capstone/logue/anal/repository/DataSourceColumnRepository.java
+++ apps/be/src/main/java/com/capstone/logue/anal/repository/DataSourceColumnRepository.java
@@ -3,5 +3,9 @@ package com.capstone.logue.anal.repository;
 import com.capstone.logue.global.entity.DataSourceColumn;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DataSourceColumnRepository extends JpaRepository<DataSourceColumn, Long> {
+
+    List<DataSourceColumn> findByDataSourceId(Long dataSourceId);
 }
