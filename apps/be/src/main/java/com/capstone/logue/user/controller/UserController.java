718cfae747d6d43eeb3c50a9c776187812fe231f
Jeein Min <clarajeein@gmail.com>
2026-04-27 17:16:19 +0900

    feat: 신규 login 시 db에 저장하는 로직 추가 (#50)
    * feat: 신규 login 시 db에 저장하는 로직 추가
    
    * chore: V1__init 구문 오류 수정

--- apps/be/src/main/java/com/capstone/logue/user/controller/UserController.java
+++ apps/be/src/main/java/com/capstone/logue/user/controller/UserController.java
@@ -15,6 +15,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
